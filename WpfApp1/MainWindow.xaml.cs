e0dd4980e12e70e44ab0b919496bdb200b9b7fae
ps_minsk <ps_minsk@mail.ru>
2018-10-03 01:03:33 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int SelectedItem;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SelectedItem = -1;
+            ViewModel vm = new ViewModel();
+            ifComboBox.ItemsSource = vm.CmbContent;
+        }
+
+        private void ifComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            string str = comboBox.SelectedValue.ToString();
+            SelectedItem = comboBox.SelectedIndex;
+            MessageBox.Show(SelectedItem.ToString());
+        }
+
+        private void ifButtonResult_Click(object sender, RoutedEventArgs e)
+        {
+            bool check = false;
+            string operand1str = ifTextBoxNumber1.Text;
+            if (SelectedItem == -1) MessageBox.Show("Select Operation");
+            else
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    class ViewModel
+    {
+        public ObservableCollection<string> CmbContent { get; private set; }
+
+        public ViewModel()
+        {
+            CmbContent = new ObservableCollection<string>
+            {
+                "+",
+                "-",
+                "*",
+                "/"
+            };
+        }
+
+    }
+}
