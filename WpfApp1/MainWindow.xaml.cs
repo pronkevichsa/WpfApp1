dbfb3dca33ee295e35e636c498cd9cbc827599b7
Sergei <ps_minsk@mail.ru>
2018-10-04 00:50:17 +0300

    Full Task 4

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,33 +21,71 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         private int SelectedItem;
+        private int SelectedItem2;
+        private ViewModel vm;
+        private char selectedChar;
+        private char selected2Char;
         public MainWindow()
         {
             InitializeComponent();
             SelectedItem = -1;
-            ViewModel vm = new ViewModel();
+            vm = new ViewModel();
             ifComboBox.ItemsSource = vm.CmbContent;
+            switchListBox.ItemsSource = vm.CmbContent;
         }
 
         private void ifComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ComboBox comboBox = (ComboBox)sender;
-            string str = comboBox.SelectedValue.ToString();
-            SelectedItem = comboBox.SelectedIndex;
-            MessageBox.Show(SelectedItem.ToString());
+            ComboBox comboBox = (ComboBox)sender;            
+            selectedChar = (char)comboBox.SelectedValue;
+            SelectedItem = comboBox.SelectedIndex;           
         }
 
         private void ifButtonResult_Click(object sender, RoutedEventArgs e)
         {
             bool check = false;
+            double result = 0;
             string operand1str = ifTextBoxNumber1.Text;
-            if (SelectedItem == -1) MessageBox.Show("Select Operation");
+            string operand2str = ifTextBoxNumber2.Text;
+            if (SelectedItem == -1) MessageBox.Show("Please, Select Operation");
             else
             {
+                bool res = vm.Calculation(operand1str, operand2str, selectedChar, out result);
+                if (res)
+                    ifTextBoxResult.Text = result.ToString();
+                else ifTextBoxResult.Text = "Error";
+            }  
+        }
 
+        private void switchListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+            ListBox listBox = (ListBox)sender;
+            selected2Char = (char)listBox.SelectedValue;
+            SelectedItem2 = listBox.SelectedIndex;  
+        }
+
+        private void switchButtonResult_Click(object sender, RoutedEventArgs e)
+        {            
+            double result = 0;
+            string operand1str = switchTextBoxNumber1.Text;
+            string operand2str = switchTextBoxNumber2.Text;
+            if (SelectedItem2 == -1) MessageBox.Show("Please, Select Operation");
+            else
+            {
+                bool res = vm.Calculation2(operand1str, operand2str, selected2Char, out result);
+                if (res)
+                    switchTextBoxResult.Text = result.ToString();
+                else switchTextBoxResult.Text = "Error";
             }
+        }
 
-
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string strIn = InputWord.Text;
+            string strOut = "";
+            for (int i = strIn.Length; i > 0; i--)
+                strOut += strIn[i-1];
+            OutputWord.Text = strOut;
         }
     }
 }
--- WpfApp1/ViewModel.cs
+++ WpfApp1/ViewModel.cs
@@ -9,17 +9,108 @@ namespace WpfApp1
 {
     class ViewModel
     {
-        public ObservableCollection<string> CmbContent { get; private set; }
+        public ObservableCollection<char> CmbContent { get; private set; }
 
         public ViewModel()
         {
-            CmbContent = new ObservableCollection<string>
+            CmbContent = new ObservableCollection<char>
             {
-                "+",
-                "-",
-                "*",
-                "/"
+                '+',
+                '-',
+                '*',
+                '/'
             };
+        }
+
+        public bool Calculation(string operand1Input,string operand2Input,char operationInput, out double doubleResult)
+        {
+            double double1Input=0;
+            double double2Input = 0;
+            bool checkResults = false;
+            doubleResult = 0;
+            bool checkValue = Double.TryParse(operand1Input, out double1Input);
+            if (checkValue)
+            {
+                checkValue = Double.TryParse(operand2Input, out double2Input);
+                if (checkValue)
+                {
+                    if (operationInput == '+')
+                    {
+                        checkResults = true;
+                        doubleResult = double1Input + double2Input;
+                    }
+                    if (operationInput == '-')
+                    {
+                        checkResults = true;
+                        doubleResult = double1Input - double2Input;
+                    }
+                    if (operationInput == '*')
+                    {
+                        checkResults = true;
+                        doubleResult = double1Input * double2Input;
+                    }
+                    if (operationInput == '/')
+                    {
+                        if (double2Input != 0)
+                        {
+                            checkResults = true;
+                            doubleResult = double1Input / double2Input;
+                        }
+                        else
+                        {
+                            checkResults = false;
+                            //throw new Exception("Something wrong");
+                        }
+                    }
+                }
+            }   
+            return checkResults;
+        }
+
+
+        public bool Calculation2(string operand1Input, string operand2Input, char operationInput, out double doubleResult)
+        {
+            double double1Input = 0;
+            double double2Input = 0;
+            bool checkResults = false;
+            doubleResult = 0;
+            bool checkValue = Double.TryParse(operand1Input, out double1Input);
+            if (checkValue)
+            {
+                checkValue = Double.TryParse(operand2Input, out double2Input);
+                if (checkValue)
+                {
+                    switch (operationInput)
+                    {
+
+                        case '+':
+                            checkResults = true;
+                            doubleResult = double1Input + double2Input;
+                            break;
+                        case '-':
+                            checkResults = true;
+                            doubleResult = double1Input - double2Input;
+                            break;
+                        case '*':
+                            checkResults = true;
+                            doubleResult = double1Input * double2Input;
+                            break;
+                        case '/':
+                            if (double2Input != 0)
+                            {
+                                checkResults = true;
+                                doubleResult = double1Input / double2Input;
+                            }
+                            else
+                            {
+                                checkResults = false;
+                                //throw new Exception("Something wrong");
+                            }
+                            break;
+                    }            
+                }
+            }
+            return checkResults;
         }
 
     }
